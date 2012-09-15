505af9c48175dd3bb4fe68a81f3556e55327c105
Pedro Carriço <pedro.carrico@gmail.com>
2012-09-15 10:22:09 +0100

    Wake up drunkbot

--- src/drinkalot/bot/drunk/DrunkBot.java
+++ src/drinkalot/bot/drunk/DrunkBot.java
@@ -14,36 +14,38 @@ public class DrunkBot extends PircBot {
     boolean mute = false;
     Random generator;
     int insultModifier;
-
-    public DrunkBot() {
-        this.setName("DrunkBot");
+    String master = null;
+    
+    public DrunkBot(String name, String botMaster) {
+        this.setName(name);
+        this.master = botMaster;
         generator = new Random();
         insultModifier = 3;
     }
-
+    
     public Calendar calculateNextFridayAt18PM() {
-
+        
         Calendar nextFridayAt18PM = Calendar.getInstance();
-
+        
         nextFridayAt18PM.set(Calendar.HOUR_OF_DAY, 18);
         nextFridayAt18PM.set(Calendar.MINUTE, 0);
         nextFridayAt18PM.set(Calendar.SECOND, 0);
-
+        
         while (nextFridayAt18PM.get(Calendar.DAY_OF_WEEK) != Calendar.FRIDAY) {
             nextFridayAt18PM.add(Calendar.DAY_OF_MONTH, 1);
         }
-
+        
         return nextFridayAt18PM;
     }
-
+    
     public String getInsult() {
         String insult = "You heathen swine";
-
+        
         try {
             URL webinsult = new URL("http://www.webinsult.com/");
             BufferedReader bf = new BufferedReader(new InputStreamReader(webinsult.openStream()));
             String inputLine;
-
+            
             while ((inputLine = bf.readLine()) != null) {
                 if (inputLine.contains("class=\"insult\" id=\"insult\"")) {
                     insult = inputLine.substring(inputLine.lastIndexOf("\">") + 2, inputLine.lastIndexOf("</div>"));
@@ -51,25 +53,30 @@ public class DrunkBot extends PircBot {
                 }
             }
             bf.close();
-            
         } catch (Exception ex) {
             //DO NOTHING
         }
-
+        
         return insult;
     }
-
+    
     private boolean insultTime() {
             return generator.nextInt(10) < insultModifier;
     }
-
+    
+    protected void onInvite(String targetNick, String sourceNick, String sourceLogin, String sourceHostname, String channel) {
+        System.out.println("Invited to " + channel);
+        if (this.master != null && sourceNick.equals(this.master)) {
+            this.joinChannel(channel);
+        }
+    }
+    
     public void onMessage(String channel, String sender, String login, String hostname, String message) {
-
+        
         String time = new java.util.Date().toString();
         Calendar now = Calendar.getInstance();
         Calendar nextFridayAt18PM = calculateNextFridayAt18PM();
-
-
+        
         if (mute) {
             if (message.toLowerCase().contains("wake up")) {
                 sendMessage(channel, sender + ", I am awake now");
@@ -81,31 +88,31 @@ public class DrunkBot extends PircBot {
                 mute = true;
                 return;
             }
-
+            
             if (message.toLowerCase().contains("more insults")) {
                 sendMessage(channel, sender + ", You're still not satisfied?!");
                 if (insultModifier < 10)
                     insultModifier+=1;
                 return;
             }
-
+            
             if (message.toLowerCase().contains("less insults")) {
                 sendMessage(channel, sender + ", I'll keep a lower profile from now on...");
                 if (insultModifier > 1)
                     insultModifier-=1;
                 return;
             }
-
+            
             if (insultTime()){
                  sendMessage(channel, sender + ", " + getInsult());
                  return;
             }
-
+            
             if (message.equalsIgnoreCase("time")) {
                 sendMessage(channel, sender + ": The time is now " + time);
                 return;
             }
-
+            
             if (message.toLowerCase().contains("jolas")) {
                 long timeLeftToBeer = (nextFridayAt18PM.getTimeInMillis() - now.getTimeInMillis()) / (1000 * 60);
                 sendMessage(channel, "Still " + timeLeftToBeer + " minutes left to drink beer :-(");
--- src/drinkalot/bot/drunk/MyBotMain.java
+++ src/drinkalot/bot/drunk/MyBotMain.java
@@ -6,21 +6,29 @@ import java.util.Properties;
 public class MyBotMain {
     
     public static void main(String[] args) throws Exception {
-
+        
         Properties p = new Properties();
         p.load(new FileInputStream("irc.properties"));
-
+        
         // Now start our bot up.
-        DrunkBot bot = new DrunkBot();
+        DrunkBot bot = new DrunkBot(p.getProperty("irc.user.name"), p.getProperty("irc.bot.master"));
         
         // Enable debugging output.
-        bot.setVerbose(true);
+        bot.setVerbose(p.getProperty("irc.bot.verbose").equals("true"));
         
         // Connect to the IRC server.
         bot.connect(p.getProperty("irc.server.location"),
                     Integer.parseInt(p.getProperty("irc.server.port")),
                     p.getProperty("irc.server.password"));
-
-        bot.joinChannel(p.getProperty("irc.server.channel"));
+        
+        String botPassword = p.getProperty("irc.user.password");
+        if (botPassword != null)
+            bot.identify(botPassword);
+        
+        String[] channels = p.getProperty("irc.server.channels").split(",");
+        
+        for (String channel : channels) {
+            bot.joinChannel(channel);
+        }
     }
 }
